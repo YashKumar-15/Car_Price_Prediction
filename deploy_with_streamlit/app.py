49bcc2f1449e3e61548c0b6d70c904e7043b3d41
Yash <70682265+Yashkumar-19@users.noreply.github.com>
2024-05-18 12:24:06 +0530

    Add files via upload

--- deploy_with_streamlit/app.py
+++ deploy_with_streamlit/app.py
@@ -1,5 +1,6 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
+import streamlit as st
 import warnings
 warnings.filterwarnings('ignore')
 from sklearn.model_selection import train_test_split
@@ -9,6 +10,8 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 import pickle
 import datetime
+
+
 
 st.header('Welcome, To the Car Price Prediction ML Model')
 st.sidebar.header('Select Car Features')
@@ -27,7 +30,7 @@ name = st.sidebar.selectbox('Select Car', df['name'].unique())
 
 
 
-year = st.selectbox("Select a year", range(1991, 2025))
+year = st.selectbox("Select a year", range(2013, 2025))
 
 st.markdown("Note: Select Year from 1991 to 2024")
 options = ['']
@@ -37,21 +40,21 @@ transmission = st.sidebar.radio('Select Transmission', df['transmission'].unique
 
 owner = st.sidebar.selectbox('Select Owner', df['owner'].unique())
 default__engine_value = 2000
-engine = st.slider('Select engine', 620, 3600,default__engine_value)
+engine = st.slider('Select engine (CC)', 700, 3600,default__engine_value)
 default_mileage_value = 12
 mileage = st.slider('Select mileage', 2, 42,default_mileage_value)
-default_km_driven_value = 110
-km_driven = st.slider('Select km Travelled',100, 25000,default_km_driven_value)
-default_max_power_value = 250
-max_power = st.slider('Select max_power', 2, 400,default_max_power_value)
+default_km_driven_value = 70000
+km_driven = st.slider('Select km Travelled',5000, 200000,default_km_driven_value)
+# default_max_power_value = 250
+# max_power = st.slider('Select max_power', 2, 400,default_max_power_value)
 
 seats = st.slider('Select Seats', 2, 14, step=1)
 
-ml_model = st.radio('Select ML Model',['Linear Reagression'])
+# ml_model = st.radio('Select ML Model',['Linear Reagression','Decision Tree'])
 if st.button('Predict'):
   df2=pd.DataFrame([[name, year, km_driven, fuel, seller_type, transmission,
-       owner, mileage, engine, max_power, seats]], columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission',
-       'owner', 'mileage', 'engine', 'max_power', 'seats'])
+       owner, mileage, engine, seats]], columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission',
+       'owner', 'mileage', 'engine', 'seats'])
   
   df2['name'] = df2['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
         'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
@@ -64,11 +67,11 @@ if st.button('Predict'):
   df2['transmission'] = df2['transmission'].replace(['Manual', 'Automatic'],[1,2])
   df2['fuel'] = df2['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'],[1,2,3,4])
 
-  if ml_model == 'Linear Reagression':
-    lr_prediction = LR_model.predict(df2)
+  # if ml_model == 'Linear Reagression':
+  lr_prediction = LR_model.predict(df2)
   #   st.write(df2)
-    st.write('Hi Predicted Price of Car with Linear Regression is ',str(format(lr_prediction[0],'.2f')))
+  st.write('Hi Predicted Price of Car is ',str(format(lr_prediction[0],'.2f')))
   # elif ml_model == 'Decision Tree':
   #   dt_prediction = DT_model.predict(df2)
-  # #   st.write(df2)
-  #   st.write('Hi Predicted Price of Car with Decision Tree is ',str(format(dt_prediction[0],'.2f')))
+  #   st.write(df2)
+    # st.write('Hi Predicted Price of Car with Decision Tree is ',str(format(dt_prediction[0],'.2f')))
